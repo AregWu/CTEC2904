dd33adc1776bfd350cfc8a2ad025a2de945bd7f6
Arkadiusz Wieckowski <p17213036@my365.dmu.ac.uk>
2018-11-29 15:10:43 +0000

    add calculator

--- calculator.c
+++ calculator.c
@@ -1,56 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/*
-* 
-*/
-int addd(int one,int two){
-return one+two;
-}
-int divii(int one,int two){
-return one/two;
-}
-int multii(int one,int two){
-return one*two;
-}
-int subss(int one,int two){
-return one-two;
-}
-int main(int argc, char** argv) {
-int one=0, two=0, cho=0, result=0;
-
-printf("\nWrite two numbers \n");
-scanf("%d %d", &one, &two);
-
-printf("Choose what to do \n");
-printf("1 - Add numbers, 2 - Divide numbers, 3 - Multiply numbers, 4 - Subtract numbers \n");
-scanf("%d", &cho);
 
 
-switch(cho){
-case 1: 
-result = addd(one, two);
-break;
-
-case 2:
-result = divii(one, two);
-break;
-
-case 3:
-result = multii(one, two); 
-break;
-
-case 4:
-result = subss(one, two); 
-break;
-
+int main(void) {
+  int hEntities;
+    printf("how many entities do you want ");
+    scanf("%d",&hEntities); 
+    int b = hEntities;
+    int d;
+    int entities;
+    printf("write your entities");
+    for (int a = 0; a<b; a++)
+    {
+    scanf("%d", &entities);
+    d = d + entities;
+    }
+    printf("%d", d/hEntities);
+  return 0;
 
 }
-
-
-
-printf("\n Your result is: %d\n", result);
-
-return (EXIT_SUCCESS);
-}
-
